8845052013fde5d219fb02044c258a798c8e88e4
Declan Fletcher <dec.fletcher@gmail.com>
2022-04-08 18:01:55 +0100

    added comments and changed workout plans heading

--- static/javascript/popups.js
+++ static/javascript/popups.js
@@ -1,9 +1,9 @@
 function confirmCreationMessage() {
-    document.getElementById("creation-message").innerHTML = "Your workout plan has been created.";
+    document.getElementById("creation-message").innerHTML = "Your workout plan has been created."; // adds text to a p tag in the html //
     var fade = document.getElementById("creation-message");
     var intervalID = setInterval(
         function () {
-            document.getElementById("plan-form").reset();
+            document.getElementById("plan-form").reset(); // clears the form // 
             if (!fade.style.opacity) {
                 fade.style.opacity = 1;
             }
@@ -12,28 +12,30 @@ function confirmCreationMessage() {
             }
             else {
                 clearInterval(intervalID);
-                location.reload();
+                location.reload(); // reloads the page // 
             }
         }
     )
 }
 
+// creates an alert box pop-up // 
 function confirmLoginMessage() {
     alert("Login Successful");
 
 }
 
+// creates an alert box pop-up //
 function confirmSignupMessage() {
     alert("Account creation has been successful.");
     
 }
 
 function confirmChangePasswordMessage() {
-    document.getElementById("password-change-message").innerHTML = "Your password has successfully been changed.";
+    document.getElementById("password-change-message").innerHTML = "Your password has successfully been changed."; // adds text to a p tag in the html //
     var fade = document.getElementById("password-change-message");
     var intervalID = setInterval(
         function () {
-            document.getElementById("change-password").reset();
+            document.getElementById("change-password").reset(); // clears the form //
             if (!fade.style.opacity) {
                 fade.style.opacity = 1;
             }
@@ -42,7 +44,7 @@ function confirmChangePasswordMessage() {
             }
             else {
                 clearInterval(intervalID);
-                location.reload();
+                location.reload(); // reloads the page // 
             }
         }
     )
